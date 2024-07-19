4d3be61be743481dc01f211030861756759b79a5
KouhTest <jeremycecchi11@gmail.com>
2024-07-19 17:59:09 -0400

    Avance 1.6
    Finalización de la Evaluación, agregando detalles visuales y un respectivo README al repositorio evaluado.

---
+++ README.md
@@ -0,0 +1,22 @@
+<h3 align="center">Evaluación Acumulativa 3</h3>
+
+---
+
+# Requerimientos
+
+El sistema debe tener las siguientes funcionalidades:
+- Se debe poder agregar notas (post it), para cada nota interesa almacenar el título, una descripción y si es importante o no.
+- Las notas importantes deben destacarse en rojo (ver figura).
+- Se deben poder eliminar notas presionando la X en la parte superior derecha de la misma.
+- El sistema debe mostrar filas de 4 post it.
+- Se debe validar el ingreso de información. El campo descripción es obligatorio, el resto es optativo.
+- Se debe utilizar react js para realizar la aplicación
+- Puede utilizar un framework de CSS si lo desea
+- Debe separar en componentes el sistema.
+- Debe subir commits del avance de su proyecto. Al menos 5 commits. No se consideraran los commits fuera de plazo. 
+
+# Equipo de trabajo
+
+- Jeremy Cecchi
+- Fabian Olcay
+- Brayan Salas
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,9 +5,26 @@ export const Posts = ({ post, RemovePost }) => {
     const {title, description, important, id} = post;
     const removePost = () => RemovePost(id);
 
+    function Rotate(rot) {
+        const ALEATORIO = Math.random();
+        return rot[Math.floor(ALEATORIO * rot.length)];
+    };
+
+    const RotationCard1 = [
+        'card importantCard rotation-1', 
+        'card importantCard rotation-2', 
+        'card importantCard rotation-3'
+    ];
+
+    const RotationCard2 = [
+        'card normalCard rotation-1', 
+        'card normalCard rotation-2', 
+        'card normalCard rotation-3'
+    ];
+
     if (important) {
         return (
-            <div className="card importantCard" name={title}>
+            <div className={Rotate(RotationCard1)} name={title}>
                 <div className="card-body">
                     <div className='flex-container'>
                         <h5 className="card-title textBold">{title}</h5>
@@ -20,7 +37,7 @@ export const Posts = ({ post, RemovePost }) => {
 
     } else {
         return (
-            <div className="card normalCard">
+            <div className={Rotate(RotationCard2)}>
                 <div className="card-body">
                     <div className='flex-container'>
                         <h5 className="card-title textBold">{title}</h5>
